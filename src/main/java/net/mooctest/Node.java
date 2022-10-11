1ebc373a6d507b16bcc6ec893e34222b9dd23d8b
phj233 <2780990934@qq.com>
2022-10-11 21:55:50 +0800

    add:`HillClimbing.java` `Node` `RandomRestart` 的中文注释

--- src/main/java/net/mooctest/Node.java
+++ src/main/java/net/mooctest/Node.java
@@ -7,22 +7,22 @@ package net.mooctest;
 import java.util.*;
 
 public class Node implements Comparable<Node>{
-	private static final int N=8; //8 queens
-	public Queen[] state; //the node's state
-	private ArrayList<Node> neighbours;
-	private int hn; //heuristic score
+	private static final int N=8; //8 queens	8个皇后棋子
+	public Queen[] state; //the node's state	节点状态
+	private ArrayList<Node> neighbours;	//相邻节点数组
+	private int hn; //heuristic score	启发分数
 	
 	/**
-	 * Constructor
+	 * Constructor	构造函数
 	 */
 	public Node(){
-		state = new Queen[N]; //empty state
-		neighbours = new ArrayList<Node>(); //empty neighbour list
+		state = new Queen[N]; //empty state	空的状态
+		neighbours = new ArrayList<Node>(); //empty neighbour list	空的相邻链表
 	} 
 	
 	/**
-	 * Constructor which creates a copy of a node's state
-	 * @param n
+	 * Constructor which creates a copy of a node's state	构造函数，它创建节点状态的副本
+	 * @param n	参数node对象 
 	 */
 	public Node(Node n){
 		state = new Queen[N];
@@ -34,8 +34,8 @@ public class Node implements Comparable<Node>{
 	
 	/**
 	 * Generates the possible chess board configurations given a
-	 * specific board state
-	 * @param startState
+	 * specific board state	在给定特定棋盘状态的情况下，生成可能的棋盘配置
+	 * @param startState	node对象 起始状态
 	 */
 	public ArrayList<Node> generateNeighbours(Node startState){
 		int count=0;
@@ -51,20 +51,20 @@ public class Node implements Comparable<Node>{
 			for(int j=1; j<N; j++){
 				neighbours.add(count, new Node(startState));
 				neighbours.get(count).state[i].moveDown(j);
-				//make sure to compute its hn value
+				//make sure to compute its hn value	确保计算它们的hn值
 				neighbours.get(count).computeHeuristic();
 				
 				count++;
 			}
 		}
 		
-		return neighbours;
+		return neighbours;		//返回相邻节点
 	}
 	
 	/**
-	 * Returns a randomly generated neighbour of a given state
+	 * Returns a randomly generated neighbour of a given state 返回随机生成的相邻节点的状态
 	 * @param startState
-	 * @return
+	 * @return	返回相邻节点 先new一个起始状态节点 然后在0-6之间随机数移动，之后计算启发式
 	 */
 	public Node getRandomNeighbour(Node startState){
 		Random gen = new Random();
@@ -81,8 +81,8 @@ public class Node implements Comparable<Node>{
 	
 	/**
 	 * computes the heuristic, which is the number of 
-	 * pieces that can attack each other
-	 * @return int
+	 * pieces that can attack each other	计算启发式(heuristic)，即可以相互攻击的棋子的数量
+	 * @return int	返回分数
 	 */
 	public int computeHeuristic(){
 	
@@ -100,7 +100,7 @@ public class Node implements Comparable<Node>{
 
 	
 	/**
-	 * Hn getter
+	 * Hn getter	获取启发分数
 	 * @return
 	 */
 	public int getHeuristic(){
@@ -109,9 +109,9 @@ public class Node implements Comparable<Node>{
 	
 	/**
 	 * Implements Comparable method compareTo, judges a comparison
-	 * on the basis of a Node's heuristic value
+	 * on the basis of a Node's heuristic value	实现比较方法compareTo，根据节点的启发式值判断比较
 	 * @param n
-	 * @return int
+	 * @return int	
 	 */
 	public int compareTo(Node n){
 		if(this.hn < n.getHeuristic())
@@ -123,7 +123,7 @@ public class Node implements Comparable<Node>{
 	}
 	
 	/**
-	 * state setter
+	 * state setter	设置状态
 	 * @param s
 	 */
 	public void setState(Queen[] s){
@@ -134,32 +134,32 @@ public class Node implements Comparable<Node>{
 	
 	
 	/**
-	 * state getter
-	 * @return
+	 * state getter	获取状态
+	 * @return	返回状态
 	 */
 	public Queen[] getState(){
 		return state;
 	}
 	
 	/**
-	 * toString method prints out Node's state
-	 * @return String
+	 * toString method prints out Node's state	toString方法打节点的状态
+	 * @return String	返回rs结果
 	 */
 	public String toString(){
 		String result="";
 		String[][] board = new String[N][N];
 		
-		//initialise board with X's to indicate empty spaces
+		//initialise board with X's to indicate empty spaces	用X初始化板以表示空白
 		for(int i=0; i<N; i++)
 			for(int j=0; j<N; j++)
 				board[i][j]="X ";
 		
 		//place the queens on the board
 		for(int i=0; i<N; i++){
-			board[state[i].getRow()][state[i].getColumn()]="Q ";
+			board[state[i].getRow()][state[i].getColumn()]="Q ";	//把q放在棋盘上
 		}
 		
-		//feed values into the result string
+		//feed values into the result string	将值输入rs结果
 		for(int i=0; i<N; i++){
 			for(int j=0; j<N; j++){
 				result+=board[i][j];
--- src/main/java/net/mooctest/RandomRestart.java
+++ src/main/java/net/mooctest/RandomRestart.java
@@ -2,24 +2,29 @@ package net.mooctest;
 /**
  * @file RandomRestart.java
  * @author Natasha Squires <nsquires@upei.ca>
- * Implements the random restart hill climbing algorithm
+ * Implements the random restart hill climbing algorithm	实现随机重启爬坡算法
  */
 public class RandomRestart {
-	private HillClimbing hillClimber;
-	private int nodesGenerated;
-	private Node start;
+	private HillClimbing hillClimber;	
+	/*爬山算法 爬山算法是一种局部择优的方法，采用启发式方法，
+	 * 是对深度优先搜索的一种改进，它利用反馈信息帮助生成解的决策。 属于人工智能算法的一种。
+	 * 爬山搜索的意思就是不断地向更高点前进，那么当然我们爬山都是一步步爬过去的，所以它是一点一点爬过去的
+	 * 它返回的是一个状态，这个状态是一个局部最值点，它只能保证返回一个局部最值点，但不能保证返回一个全局最值点。
+	 * */
+	private int nodesGenerated;	//节点生成
+	private Node start;	//起始节点
 	
 	/**
 	 * Constructor
 	 */
 	public RandomRestart(Queen[] startBoard){
 		hillClimber = new HillClimbing(startBoard);
-		nodesGenerated = 0;
+		nodesGenerated = 0;	//节点生成数
 	}
 	
 	/**
-	 * The random restart hill climbing algorithm
-	 * @return
+	 * The random restart hill climbing algorithm	随机重启爬坡算法
+	 * @return		返回当前节点
 	 */
 	public Node randomRestart(){
 		Node currentNode = hillClimber.getStartNode();
@@ -40,16 +45,16 @@ public class RandomRestart {
 	}
 	
 	/**
-	 * Sets the initial board
-	 * @param n
+	 * Sets the initial board	设置初始棋盘
+	 * @param n		参数 节点
 	 */
 	public void setStartNode(Node n){
 		start = n;
 	}
 	
 	/**
-	 * Start set getter
-	 * @return Node
+	 * Start set getter	开始设置获取
+	 * @return Node	返回开始节点
 	 */
 	public Node getStartNode(){
 		return start;
@@ -57,8 +62,8 @@ public class RandomRestart {
 	
 	/**
 	 * Returns the amount of nodes generated during the 
-	 * random restart algorithm
-	 * @return int
+	 * random restart algorithm	返回随机重启算法期间生成的节点数量
+	 * @return int	返回已生成节点数
 	 */
 	public int getNodesGenerated(){
 		return nodesGenerated;
